786b08a4c39826266cd50867c8a49a5097ce15cc
kennedimalheiros <kennedimalheiros@gmail.com>
2012-03-20 02:08:43 -0300

    Criação e Encapsulamento classe Vendas

---
+++ src/br/edu/fasa/vendas/domainmodel/Vendas.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.vendas.domainmodel;
+
+import java.util.Date;
+
+/**
+ *
+ * @author www
+ */
+public class Vendas {
+
+    private int cod;
+    private Cliente cliente;
+    private Date data;
+
+    public void Vendas() {
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
