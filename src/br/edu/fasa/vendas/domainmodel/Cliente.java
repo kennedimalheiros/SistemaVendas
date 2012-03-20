6bd36ee2ea34466d7d28ae4498feec3b292527aa
kennedimalheiros <kennedimalheiros@gmail.com>
2012-03-20 02:01:08 -0300

    Iniciando SistemaVendas

---
+++ src/br/edu/fasa/vendas/domainmodel/Cliente.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.vendas.domainmodel;
+
+/**
+ *
+ * @author www
+ */
+public class Cliente {
+    private int cod;
+    private String nome;
+    
+    public void Cliente(){
+    
+}
+    
+}
