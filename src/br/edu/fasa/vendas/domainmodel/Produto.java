737c08f944219e0fa6eef6741cb6d677550d110f
kennedimalheiros <kennedimalheiros@gmail.com>
2012-03-20 02:11:33 -0300

    Criação Classe Produtos

---
+++ src/br/edu/fasa/vendas/domainmodel/Produto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.vendas.domainmodel;
+
+/**
+ *
+ * @author www
+ */
+public class Produto {
+    private int cod;
+    private String nome;
+    private float valor;
+    private int estoque;
+    
+}
