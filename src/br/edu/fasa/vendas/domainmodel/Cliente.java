9cee90ee5c572d1483d866c1b13b5fdd3e08fa7f
kennedimalheiros <kennedimalheiros@gmail.com>
2012-03-20 02:04:59 -0300

    Encapsulamento classe Cliente

--- src/br/edu/fasa/vendas/domainmodel/Cliente.java
+++ src/br/edu/fasa/vendas/domainmodel/Cliente.java
@@ -9,11 +9,26 @@ package br.edu.fasa.vendas.domainmodel;
  * @author www
  */
 public class Cliente {
+
     private int cod;
     private String nome;
-    
-    public void Cliente(){
-    
-}
-    
+
+    public void Cliente() {
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
