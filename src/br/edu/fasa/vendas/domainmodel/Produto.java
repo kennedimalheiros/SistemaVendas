9a45ad33b343f76fa101b344300ad53fac13d5e9
kennedimalheiros <kennedimalheiros@gmail.com>
2012-03-20 02:12:26 -0300

    Encapsulamento da Classe Produtos

--- src/br/edu/fasa/vendas/domainmodel/Produto.java
+++ src/br/edu/fasa/vendas/domainmodel/Produto.java
@@ -9,9 +9,44 @@ package br.edu.fasa.vendas.domainmodel;
  * @author www
  */
 public class Produto {
+
     private int cod;
     private String nome;
     private float valor;
     private int estoque;
-    
+
+    public void Produto() {
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
 }
